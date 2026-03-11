35a25675bc9092ce62cdcacd1e46ffe323f0a1dc
jakub kleman <jakub.kleman@gmail.com>
2026-03-11 11:36:25 +0100

    Add Trajectory class to calculate and plot robot's track center

---
+++ zadanie1/include/Trajectory.hpp
@@ -0,0 +1,13 @@
+//
+// Created by kllemo on 3/11/26.
+//
+
+#ifndef ZADANIE1_TRAJECTORY_HPP
+#define ZADANIE1_TRAJECTORY_HPP
+
+
+class Trajectory {
+};
+
+
+#endif //ZADANIE1_TRAJECTORY_HPP
---
+++ zadanie1/src/Trajectory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kllemo on 3/11/26.
+//
+
+#include "../include/Trajectory.hpp"
